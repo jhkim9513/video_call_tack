23732a8f857eed9e3b847da28d5492624a07f212
dondon9513 <dondon9513@gmail.com>
2021-10-22 18:06:34 +0900

    setting main

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const middleWare = [logger, thunk];
+
+const store = configureStore(
+  { reducer: rootReducer },
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+
+export default store;
